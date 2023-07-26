3ec65fac3e57605a77269518b65fb68f9a29cc3f
Cpietro87 <cristianpietro2019@gmail.com>
2023-07-26 17:57:16 -0300

    list notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import NoteForm from "@/components/noteForm";
 
-export default function Home() {
+async function loadNotes() {
+  const res = await fetch("http://localhost:3000/api/notes");
+  const data = await res.json(); // conviento res en en un formato json
+  return data;
+}
+export default async function Home() {
+  const notes = await loadNotes();
+  console.log(notes);
   return (
-    <div>Home</div>
-  )
+    <div className="flex items-center justify-center h-screem">
+      <div>
+        <NoteForm />
+        {notes.map((note) => (
+          <div key={note.id} className="bg-slate-400 p-4 m-2">
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
