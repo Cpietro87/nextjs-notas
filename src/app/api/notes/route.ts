4c251c7a51d0de6071366c327a9908d5a41eb213
Cpietro87 <cristianpietro2019@gmail.com>
2023-07-26 14:13:03 -0300

    prisma querys

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,12 +1,44 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-    return NextResponse.json({
-        mesagge: "Getting Notes",
-    })
+export async function GET() {
+  try {
+    const notes = await prisma.note.findMany();
+    return NextResponse.json(notes);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
 }
-export function POST() {
-    return NextResponse.json({
-        mesagge: "creating Note ...",
-    })
+export async function POST(request: Request) {
+  try {
+    const { title, content } = await request.json();
+
+    const newNote = await prisma.note.create({
+      data: {
+        title: title,
+        content: content,
+      },
+    });
+    return NextResponse.json(newNote);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
 }
